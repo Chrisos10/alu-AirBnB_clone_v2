b6081d755e0927ec8260653756bfbd1d693c1600
chrisos10 <j.dufitumuk@alustudent.com>
2024-02-24 06:32:08 -0800

    Checkers for 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-Starting a Flask app
-"""
 
-from flask import Flask, render_template
+"""Starts a Flask web application"""
+
 from models import storage
 from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def state_list():
-    """doc
-    """
-    states = storage.all("State").values().sort(key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+def states():
+    """Comment"""
+    states = storage.all('State').values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-def tear_down(exeception):
-    """force closure of the current db session"""
+def teardown(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
